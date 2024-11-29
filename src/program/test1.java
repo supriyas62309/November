3aee231c454c5637e0a8f8217b3614bd859cd516
WIN10 <WIN10@DESKTOP-20UHEP8>
2024-11-29 21:00:59 +0530

    sssss

--- src/program/test1.java
+++ src/program/test1.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test1 {
 	public static void main(String[] args) throws InterruptedException
